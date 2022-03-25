ffcf9693f0e00a890049cd1f011ce0342443e3f9
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-03-25 13:39:07 -0300

    Requisito 4 - OK - Testes para o component NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testes do Component NotFound', () => {
+  it('Verifica se a msg de NotFound é renderizada', () => {
+    renderWithRouter(<NotFound />);
+    const h2NotFound = screen.getByRole(
+      'heading', { name: /Page requested not found/i, level: 2 },
+    );
+    expect(h2NotFound).toBeInTheDocument();
+  });
+
+  it('Verifica se a img do pikachu é renderizada', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
