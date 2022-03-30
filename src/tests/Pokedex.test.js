15a777711316207afb63ee183fbe8ee4c8f4e8de
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-03-29 21:20:58 -0300

    Requisito 5 - OK - Testes do componente Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,85 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testes do component Pokedex', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  it('Verifica se o h2 com "Encountred Pokémons" é renderizado', () => {
+    const h2Home = screen.getByRole('heading', {
+      name: /encountered pokémons/i, level: 2,
+    });
+
+    expect(h2Home).toBeInTheDocument();
+  });
+
+  it('Verifica se o proximo pokemon é exibido quando o button é clicado', () => {
+    const btnNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(btnNextPokemon).toBeInTheDocument();
+    userEvent.click(btnNextPokemon);
+
+    const pokemon = screen.getByTestId('pokemon-name');
+
+    expect(pokemon).toHaveTextContent('Charmander');
+
+    userEvent.click(btnNextPokemon);
+    expect(pokemon).toHaveTextContent('Caterpie');
+  });
+
+  it('Verifica se é exibido um pokemon por vez', () => {
+    const btnNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const pokemon = screen.getAllByTestId('pokemon-name');
+
+    userEvent.click(btnNextPokemon);
+    expect(pokemon).toHaveLength(1);
+    userEvent.click(btnNextPokemon);
+    expect(pokemon).toHaveLength(1);
+  });
+
+  it('Verifica se a Pokedex tem botões de filtro', () => {
+    const typesBtn = screen.getAllByTestId('pokemon-type-button');
+    const typesOfPokemons = [
+      'Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon',
+    ];
+
+    typesBtn.forEach((type, index) => {
+      expect(type).toHaveTextContent(typesOfPokemons[index]);
+    });
+  });
+
+  it(`Verifica se a partir da seleção de um botão de tipo, 
+    a Pokédex deve circular somente pelos pokémons daquele tipo`, () => {
+    const btnNextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
+    const btnFire = screen.getByRole('button', { name: 'Fire' });
+    const btnPsychic = screen.getByRole('button', { name: 'Psychic' });
+    const type = screen.getByTestId('pokemon-type');
+
+    userEvent.click(btnFire);
+    expect(type).toHaveTextContent('Fire');
+    userEvent.click(btnNextPokemon);
+    expect(type).toHaveTextContent('Fire');
+
+    userEvent.click(btnPsychic);
+    expect(type).toHaveTextContent('Psychic');
+    userEvent.click(btnNextPokemon);
+    expect(type).toHaveTextContent('Psychic');
+  });
+
+  it('Verifica se ao clicar no botão All os pokemons são mostrados sem filtro', () => {
+    const btnAll = screen.getByRole('button', { name: /all/i });
+    const pokemonName = screen.getByTestId(/pokemon-name/);
+    const btnNextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
+
+    userEvent.click(btnAll);
+    expect(pokemonName).toHaveTextContent('Pikachu');
+    const newPokemons = pokemons.filter(({ name }) => name !== 'Pikachu');
+    newPokemons.forEach(({ name }) => {
+      userEvent.click(btnNextPokemon);
+      expect(pokemonName).toHaveTextContent(name);
+    });
+  });
+});
